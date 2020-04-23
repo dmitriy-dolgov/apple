b76d9d8c377a3f6efeade77cfe9237182b117ecd
dmitriy-dolgov <TwilightTower@mail.ru>
2020-04-23 21:58:06 +0300

    состояние Rotten добавлено

---
+++ common/models/items/state/Rotten.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace common\models\items\state;
+
+use common\models\items\common;
+
+class Rotten extends \common\models\items\State
+{
+    /** @var integer когда сгнило */
+    protected $rotTime;
+
+
+    public function __construct(common\models\Fruit $fruit, $rotTime)
+    {
+        parent::__construct($fruit, $rotTime);
+
+        $this->rotTime = $rotTime;
+    }
+
+    public function getFunctions()
+    {
+        return [];
+    }
+
+    public function getData()
+    {
+        return [
+            [
+                'description' => 'Когда сгнило',
+                'value' => $this->rotTime,
+            ],
+        ];
+    }
+}
