609aaeeab37c3c1ba4ec9a5368d4134882703eea
dmitriy-dolgov <TwilightTower@mail.ru>
2020-04-23 21:34:04 +0300

    работа с состояниями

--- common/models/items/state/OnGround.php
+++ common/models/items/state/OnGround.php
@@ -2,21 +2,35 @@
 
 namespace common\models\items\state;
 
-class OnGround implements common\models\items\IState
+use common\models\items\common;
+
+class OnGround extends \common\models\items\State
 {
     protected $size = 100;
 
-    /** @var integer время созревания (появления) */
-    protected $ripen;
+    /** @var integer когда упал */
+    protected $fellTime;
 
+    /** @var integer текущее время */
+    protected $currentTime;
+
+    /** @var float|int как быстро сгниет */
     const ROT_TIMEOUT = 60 * 60 * 5;
 
+
+    public function __construct(common\models\Fruit $fruit, $fellTime)
+    {
+        parent::__construct($fruit, $fellTime);
+
+        $this->fellTime = $fellTime;
+        $this->currentTime = $this->fellTime;
+    }
 
     public function getFunctions()
     {
         return [
             'eat' => [
-                'name' => 'Съесть',
+                'name' => 'Откусить',
                 'params' => 'percent',
                 'func' => function ($eaten) {
                     $newSize = $this->size - $eaten;
@@ -30,13 +44,27 @@ class OnGround implements common\models\items\IState
                 'name' => 'Увеличить время',
                 'params' => 'time',
                 'func' => function ($time) {
-                    $this->ripen += $time;
-                    $this->size = ($newSize < 0) ? 0 : $newSize;
-                    if ($this->size == 0) {
-                        throw new SignalRemove();
+                    $this->currentTime += $time;
+                    $rotAfter = $this->fellTime + self::ROT_TIMEOUT;
+                    if ($this->currentTime >= $rotAfter) {
+                        return new Rotten($this->fruit, $rotAfter);
                     }
                 },
             ],
+        ];
+    }
+
+    public function getData()
+    {
+        return [
+            [
+                'description' => 'Сколько процентов осталось',
+                'value' => $this->size,
+            ],
+            [
+                'description' => 'Когда упал',
+                'value' => $this->fellTime,
+            ]
         ];
     }
 }
--- common/models/items/state/OnTree.php
+++ common/models/items/state/OnTree.php
@@ -4,25 +4,16 @@ namespace common\models\items\state;
 
 use common\models\items\common;
 
-class OnTree extends common\models\items\State
+class OnTree extends \common\models\items\State
 {
-    /** @var integer когда упал */
-    protected $fell;
-
-
-    public function __construct(common\models\Fruit $fruit)
-    {
-        parent::__construct($fruit);
-    }
-
     public function getFunctions()
     {
         return [
             'fall' => [
                 'name' => 'Упасть на землю',
-                'params' => false,
-                'func' => function () {
-                    return new OnGround();
+                'params' => 'datetime',
+                'func' => function ($datetime) {
+                    return new OnGround($this->fruit, $datetime);
                 },
             ],
         ];
